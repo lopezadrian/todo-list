1023892c1bbe600b12a6cbb836fd5f3ed06d407d
Adrian Lopez <adallobi@gmail.com>
2022-10-31 16:47:24 -0400

    Cleaned up makeHtml

--- src/makeHtml.js
+++ src/makeHtml.js
@@ -34,56 +34,8 @@ function todoListForm() {
     makeFormDiv('description', 'text', 'Description');
     
     makeFormDiv('dueDate', 'date', 'Due date');
-/* 
-    const titleFormDiv = document.createElement('div');
-    titleFormDiv.classList.add('title');
-    todoForm.appendChild(titleFormDiv);
 
-    const titleFormLabel = document.createElement('label');
-    titleFormLabel.for = 'title';
-    titleFormLabel.textContent = 'Task';
-    titleFormDiv.appendChild(titleFormLabel);
-
-    const titleFormInput = document.createElement('input');
-    titleFormInput.type = 'text';
-    titleFormInput.id = 'title';
-    titleFormInput.name = 'title';
-    titleFormInput.required = true;
-    titleFormDiv.appendChild(titleFormInput);
-    
-
-    const descriptionFormDiv = document.createElement('div');
-    titleFormDiv.classList.add('description');
-    todoForm.appendChild(descriptionFormDiv);
-
-    const descriptionFormLabel = document.createElement('label');
-    descriptionFormLabel.for = 'description';
-    descriptionFormLabel.textContent = 'Description';
-    descriptionFormDiv.appendChild(descriptionFormLabel);
-
-    const descriptionFormInput = document.createElement('input');
-    descriptionFormInput.type = 'text';
-    descriptionFormInput.id = 'description';
-    descriptionFormInput.name = 'description';
-    descriptionFormInput.required = true;
-    descriptionFormDiv.appendChild(descriptionFormInput);
-
-
-    const dueDateFormDiv = document.createElement('div');
-    dueDateFormDiv.classList.add('dueDate');
-    todoForm.appendChild(dueDateFormDiv);
-
-    const dueDateFormLabel = document.createElement('label');
-    dueDateFormLabel.for = 'dueDate';
-    dueDateFormLabel.textContent = 'Due date';
-    dueDateFormDiv.appendChild(dueDateFormLabel);
-
-    const dueDateFormInput = document.createElement('input');
-    dueDateFormInput.type = 'date';
-    dueDateFormInput.id = 'dueDate';
-    dueDateFormInput.name = 'dueDate';
-    dueDateFormInput.required = true;
-    dueDateFormDiv.appendChild(dueDateFormInput); */
+    makeFormDiv('priority', 'text', 'Priority')
 }
 
 export default todoListForm;
