d09f13a0aa3b70780a8b033439146c72921924d5
Adrian Lopez <adallobi@gmail.com>
2022-11-04 01:01:50 -0400

    Finished form creation code

--- src/makeHtml.js
+++ src/makeHtml.js
@@ -1,3 +1,8 @@
+function capitalizeString (string) {
+    const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+    return capitalizedString;
+}
+
 function makeFormDiv(inputName, inputType, labelText, isRequired) {
     const todoForm = document.getElementById('todo-form');
 
@@ -15,12 +20,57 @@ function makeFormDiv(inputName, inputType, labelText, isRequired) {
     formInput.type = inputType;
     formInput.id = inputName;
     formInput.name = inputName;
-    formInput.required = true;
+    formInput.required = isRequired;
     formDiv.appendChild(formInput);
 }
 
-function makeDropDownList() {
+function makeDropDownList(inputName, labelText, ...options) {
+    const todoForm = document.getElementById('todo-form');
 
+    const formDiv = document.createElement('div');
+    formDiv.classList.add(inputName);
+    todoForm.appendChild(formDiv);
+
+    const formLabel = document.createElement('label');
+    formLabel.for = inputName;
+    const formLabelText = document.createTextNode(labelText);
+    formLabel.appendChild(formLabelText);
+    formDiv.appendChild(formLabel);
+
+    const selectList = document.createElement('select');
+    selectList.name = inputName;
+    selectList.id = inputName;
+    formDiv.appendChild(selectList);
+
+    const defaultOption = document.createElement('option');
+    defaultOption.value = '';
+    const defaultOptionText = document.createTextNode('Select option');
+    defaultOption.appendChild(defaultOptionText);
+    selectList.appendChild(defaultOption);
+
+    for (const option of options) {
+        const selectOption = document.createElement('option');
+        selectOption.value = option;
+
+        const capitalizedOption = capitalizeString(option);
+
+        const selectOptionText = document.createTextNode(capitalizedOption);
+        selectOption.appendChild(selectOptionText);
+        
+        selectList.appendChild(selectOption);
+    }
+}
+
+function makeSubmitButton() {
+    const todoForm = document.getElementById('todo-form');
+
+    const submitButton = document.createElement('button');
+    submitButton.type = 'submit';
+    submitButton.id = 'submit';
+    const submitButtonText = document.createTextNode('Submit');
+    submitButton.appendChild(submitButtonText);
+
+    todoForm.appendChild(submitButton);
 }
 
 function todoListForm() {
@@ -45,7 +95,9 @@ function todoListForm() {
 
     makeFormDiv('steps', 'text', 'Steps', false);
 
-    makeDropDownList();
+    makeDropDownList('priority', 'Task priority', 'high', 'medium', 'low');
+
+    makeSubmitButton();
 }
 
 export default todoListForm;
