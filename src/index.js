b49460bb4cdd535b7c14a7979f94a7c6c024e455
Adrian Lopez <adallobi@gmail.com>
2022-11-08 12:03:57 -0500

    Started todo form reading

--- src/index.js
+++ src/index.js
@@ -22,3 +22,12 @@ const defaultProject = new Project();
 const defaultTask = new Task();
 
 todoListForm();
+
+function addTodo(e) {
+    e.preventDefault();
+    const formDate = new FormData(e.target);
+    const formProps = Object.fromEntries(formData);
+    newTask = new Task(formProps.title, formProps.description, formProps.dueDate, formProps.priority)
+}
+
+document.addEventListener('click', addTodo);
